93e9dae91c3df6d310126150dca3ebd551fccb04
Freya Aino <freya.aino@pm.me>
2026-08-20 17:28:18 +0200

    added TASK and config

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def main():
     import config_loader
+    import utils
 
     config = config_loader.load_config_json("./configs/train_config.json")
 
-    print(config)
+    TASK = "Mjlab-Tracking-Flat-Unitree-G1-No-State-Estimation"
+    # train_config = TrainConfig.from_task(TASK)
+
+    utils.launch_training(task_id=TASK, args=config)
 
 
 if __name__ == "__main__":
